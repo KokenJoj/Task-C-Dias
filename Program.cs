da4231f750cab3cd151d0bd77534a7e866b146dd
Dias Zholgali <zolgalidias@gmail.com>
2026-02-21 03:01:26 +0500

    Dias Zholgali task C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Product Name: ");
+        string productName = Console.ReadLine();
+
+        Console.Write("Quantity: ");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Price per Item: ");
+        double price = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Discount Percentage: ");
+        double discount = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Delivery: ");
+        double delivery = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Distance in km: ");
+        double distance = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Payment Method: ");
+        string payment = Console.ReadLine();
+
+        double totalPrice = quantity * price;
+        double discountAmount = totalPrice * discount / 100;
+        double finalPrice = totalPrice - discountAmount + delivery*distance;
+
+        Console.WriteLine("Total price: " + totalPrice);
+        Console.WriteLine("Discount amount: " + discountAmount);
+        Console.WriteLine("Final price after discount: " + finalPrice);
+    }
+}
